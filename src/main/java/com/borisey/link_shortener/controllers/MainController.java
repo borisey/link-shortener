1c4e1a552ccabc86e79868d35dc05daa06bdd51c
borisey <slamsnv@gmail.com>
2024-11-02 21:34:16 +0300

    add linkRedirect

--- src/main/java/com/borisey/link_shortener/controllers/MainController.java
+++ src/main/java/com/borisey/link_shortener/controllers/MainController.java
@@ -1,11 +1,15 @@
 package com.borisey.link_shortener.controllers;
 
-import com.borisey.link_shortener.models.Link;
 import com.borisey.link_shortener.repo.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.net.URI;
 
 @Controller
 public class MainController {
@@ -16,6 +20,14 @@ public class MainController {
     @GetMapping("/")
     public String linkAdd(Model model) {
         return "link-add";
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Object> linkRedirect(@PathVariable(value = "shortUrl") String shortUrl, Model model) {
+
+        System.out.println(shortUrl);
+
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create("https://ya.ru/")).build();
     }
 
 }
