b4ecc92d92788b9ab5939e1d8c6b3eae93a796a5
borisey <slamsnv@gmail.com>
2024-11-20 19:29:55 +0300

    fix cookee

--- src/main/java/com/borisey/link_shortener/controllers/MainController.java
+++ src/main/java/com/borisey/link_shortener/controllers/MainController.java
@@ -29,7 +29,7 @@ public class MainController {
         return "link-add";
     }
 
-    @RequestMapping(path = "/logout", method = RequestMethod.GET)
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
     public String logout(HttpServletRequest request, HttpServletResponse response, Model model) throws MalformedURLException {
 
         // Получаю хост сайта
